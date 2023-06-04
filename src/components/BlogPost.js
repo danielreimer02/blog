bff676d0543d62c7916881be8e61986347f7082f
Daniel Reimer <>
2023-06-03 23:19:56 -0400

    test cms form

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,11 +1,28 @@
-// src/components/BlogPost.js
-import React from 'react';
+import React from "react";
 
-const BlogPost = ({ title, content }) => {
+const BlogPost = ({ title, date, body, video }) => {
   return (
-    <div className="blog-post">
+    <div calssName="blog-post">
       <h2>{title}</h2>
-      <p>{content}</p>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
+
+      {video && (
+        <div>
+          <h3>Video</h3>
+          <div className="video-container">
+            <iframe
+              title="YouTube video"
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${video}`}
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
